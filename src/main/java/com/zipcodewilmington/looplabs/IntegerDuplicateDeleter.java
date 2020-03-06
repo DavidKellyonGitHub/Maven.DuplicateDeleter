f6fb1b09913a05f1709949200fcc7ac9d8b0eb4d
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-06 17:16:24 -0500

    revisit idempotence

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -33,8 +33,8 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
             resultArray[sizeCounter] = holderArray[sizeCounter];
             sizeCounter++;
         }
-
         return resultArray;
+
     }
 
     @Override
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +14,55 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        int occurenceCounter = 0;
+        int sizeCounter = 0;
+        String[] holderArray = new String[array.length];
+        for (String str : array) {
+            occurenceCounter = 0;
+            for (String str2 : array) {
+                if (str.equals(str2)) {
+                    occurenceCounter++;
+                }
+            }
+            if (occurenceCounter < maxNumberOfDuplications) {
+                holderArray[sizeCounter] = str;
+                sizeCounter++;
+            }
+        }
+        String[] resultArray = new String[sizeCounter];
+        sizeCounter = 0;
+        while (holderArray[sizeCounter] != null){
+            resultArray[sizeCounter] = holderArray[sizeCounter];
+            sizeCounter++;
+        }
+
+        return resultArray;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        int occurenceCounter = 0;
+        int sizeCounter = 0;
+        String[] holderArray = new String[array.length];
+        for (String str : array) {
+            occurenceCounter = 0;
+            for (String str2 : array) {
+                if (str.equals(str2)) {
+                    occurenceCounter++;
+                }
+            }
+            if (occurenceCounter != exactNumberOfDuplications) {
+                holderArray[sizeCounter] = str;
+                sizeCounter++;
+            }
+        }
+        String[] resultArray = new String[sizeCounter];
+        sizeCounter = 0;
+        while (holderArray[sizeCounter] != null){
+            resultArray[sizeCounter] = holderArray[sizeCounter];
+            sizeCounter++;
+        }
+
+        return resultArray;
     }
 }
