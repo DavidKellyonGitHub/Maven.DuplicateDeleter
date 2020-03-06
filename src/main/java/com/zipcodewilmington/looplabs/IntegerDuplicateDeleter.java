c5fdc1a632edf0fd356b860bcd5b23c12b78eab7
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-06 15:34:10 -0500

    int.dup.deleter less idempotence

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,62 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] resultArray = new Integer[]{};
+        int occurenceCounter = 0;
+        int sizeCounter = 0;
+        Integer[] holderArray = new Integer[array.length];
+        for (Integer num : array) {
+            occurenceCounter = 0;
+            for (Integer num2 : array) {
+                if (num.equals(num2)) {
+                    occurenceCounter++;
+                }
+            }
+            if (occurenceCounter < maxNumberOfDuplications) {
+                holderArray[sizeCounter] = num;
+                sizeCounter++;
+            }
+        }
+        resultArray = new Integer[sizeCounter];
+        sizeCounter = 0;
+        while (holderArray[sizeCounter] != null){
+            resultArray[sizeCounter] = holderArray[sizeCounter];
+            sizeCounter++;
+        }
+
+        return resultArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int occurenceCounter = 0;
+        int sizeCounter = 0;
+        Integer[] holderArray = new Integer[array.length];
+        for (Integer num : array) {
+            occurenceCounter = 0;
+            for (Integer num2 : array) {
+                if (num.equals(num2)) {
+                    occurenceCounter++;
+                }
+            }
+            if (occurenceCounter != exactNumberOfDuplications) {
+                holderArray[sizeCounter] = num;
+                sizeCounter++;
+            }
+        }
+        Integer[] resultArray = new Integer[sizeCounter];
+        sizeCounter = 0;
+        while (holderArray[sizeCounter] != null){
+            resultArray[sizeCounter] = holderArray[sizeCounter];
+            sizeCounter++;
+        }
+
+        return resultArray;
+    }
 }
